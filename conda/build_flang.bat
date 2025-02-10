95e2e05ded984e14abc6f462214dba18d6f08650
krande <kristoffer_andersen@outlook.com>
2025-02-10 07:26:09 +0100

    minor update

--- conda/build_flang.bat
+++ conda/build_flang.bat
@@ -6,30 +6,32 @@ if not exist build mkdir build
 set "LIBRARY_PREFIX=%CONDA_PREFIX%\Library"
 echo "LIBRARY_PREFIX: %LIBRARY_PREFIX%"
 
-set EXTRA_INCLUDE=%VCToolsInstallDir%\atlmfc\include
-
 :: append extra include path
 
 REM remove GL flag for now
-set "CXXFLAGS=-MD /I\"%VCToolsInstallDir%atlmfc\include\""
+set "CXXFLAGS=-MD"
 set "CC=cl.exe"
 set "CXX=cl.exe"
 
 
 cmake -G Ninja -B build -S llvm ^
-  -DCMAKE_BUILD_TYPE=Release ^
-  -DCMAKE_PREFIX_PATH=%LIBRARY_PREFIX% ^
-  -DCMAKE_INSTALL_PREFIX=%LIBRARY_PREFIX% ^
-  -DCMAKE_MSVC_RUNTIME_LIBRARY=MultiThreadedDLL ^
-  -DCMAKE_CXX_STANDARD=17 ^
-  -DCMAKE_EXPORT_COMPILE_COMMANDS=ON ^
-  -DLLVM_ENABLE_ASSERTIONS=ON ^
-  -DLLVM_TARGETS_TO_BUILD=host ^
-  -DLLVM_INCLUDE_BENCHMARKS=OFF ^
-  -DLLVM_INCLUDE_DOCS=OFF ^
-  -DLLVM_LIT_ARGS=-v ^
-  -DLLVM_ENABLE_PROJECTS="clang;mlir;flang;openmp" ^
-  -DLLVM_ENABLE_RUNTIMES="compiler-rt"
+    -DCMAKE_BUILD_TYPE=Release ^
+    -DCMAKE_PREFIX_PATH=%LIBRARY_PREFIX% ^
+    -DCMAKE_INSTALL_PREFIX=%LIBRARY_PREFIX% ^
+    -DCMAKE_MSVC_RUNTIME_LIBRARY=MultiThreadedDLL ^
+    -DCMAKE_CXX_STANDARD=17 ^
+    -DCMAKE_EXPORT_COMPILE_COMMANDS=ON ^
+    -DLLVM_ENABLE_ASSERTIONS=ON ^
+    -DLLVM_TARGETS_TO_BUILD=host ^
+    -DLLVM_INCLUDE_BENCHMARKS=OFF ^
+    -DLLVM_INCLUDE_DOCS=OFF ^
+    -DLLVM_INCLUDE_TESTS=ON ^
+    -DLLVM_INCLUDE_UTILS=ON ^
+    -DLLVM_INSTALL_UTILS=ON ^
+    -DLLVM_USE_SYMLINKS=OFF ^
+    -DLLVM_LIT_ARGS=-v ^
+    -DLLVM_ENABLE_PROJECTS="clang;mlir;flang;openmp" ^
+    -DLLVM_ENABLE_RUNTIMES="compiler-rt"
 
 
 ninja -C build install
