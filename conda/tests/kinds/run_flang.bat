1ba64e2f0992929b7affa0bc8cd658ecb23e8720
krande <kristoffer_andersen@outlook.com>
2025-02-18 07:07:33 +0100

    fix flang kinds test

--- conda/tests/kinds/run_flang.bat
+++ conda/tests/kinds/run_flang.bat
@@ -8,6 +8,6 @@ if not exist %THIS_DIR%out mkdir %THIS_DIR%out
 
 flang-new.exe %THIS_DIR%cmake_kinds_check.F90 -o %THIS_DIR%out/cmake_kinds_check.exe
 
-call %THIS_DIR%out\kinds_check.exe
+call %THIS_DIR%out\cmake_kinds_check.exe
 
 endlocal
